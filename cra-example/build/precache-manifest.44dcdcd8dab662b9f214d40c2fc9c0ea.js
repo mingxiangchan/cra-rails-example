e3eed75f67c616d34236c2283930e8c9dbeaa6f2
Chan Ming Xiang <mingxiangchan@gmail.com>
2018-11-05 18:07:53 +0800

    ran `npm run build` on cra

---
+++ cra-example/build/precache-manifest.44dcdcd8dab662b9f214d40c2fc9c0ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "173cf8f081f01ac4e26b",
+    "url": "/static/js/main.173cf8f0.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f744f6ccbd4e147b2707caeb93f45191",
+    "url": "/index.html"
+  }
+];
